1567b2fcc80ab56b33b403161982a0145ee760d2
Pranav K V N <kvnp179@gmail.com>
2021-11-14 00:38:48 +0530

    x

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/index.css">
         <link rel="icon" href="img/leaves.png">
-        <script src="js/dropdown.js" type="application/javascript"></script>
         <title>Medicbot</title>
         
       <!-- 
