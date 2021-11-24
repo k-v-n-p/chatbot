f82984afea0882df80e21e12b8d574aeec6b7d1d
Prateek Mishra <justwriteprateek@gmail.com>
2021-11-24 20:49:46 +0530

    added content on index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 
     <body>
-        
+      <div id="front-page-gradient">
         <?php include 'partials/header.php'; ?>
 
         <div class="container">
@@ -23,33 +23,48 @@
              
             </div>
             <div class="col-lg-6 main-page-container">
-              <img src="img/medicbot.png" alt="">
+              <img src="img/medicbot.png" alt="medicbot logo">
             </div>
           </div>
         </div>
+      </div>
+      <div id="block-2">
+      <div class="container">
+          <div class="row">
+            <div class="col-lg-6  main-page-content">
+            
+              <div class="d-flex flex-column bd-highlight mb-3">
+                <div class="p-2 bd-highlight"><h1>Medicbot</h1></div>
+                <div class="p-2 bd-highlight"><h3>For Patients</h3></div>
+                
+              </div>
+             
+            </div>
+            <div class="col-lg-6 main-page-content">
+              <p>Our AI model is trained to identify common diseases<br> and provide medication and advice</p>
+            </div>
+          </div>
+      </div>
 
-       
-        <script>!(function () {
-            let e = document.createElement("script"),
-              t = document.head || document.getElementsByTagName("head")[0];
-            (e.src =
-              "https://cdn.jsdelivr.net/npm/rasa-webchat/lib/index.js"),
-              // Replace 1.x.x with the version that you want
-              (e.async = !0),
-              (e.onload = () => {
-                window.WebChat.default(
-                  {
-                    initPayload: '/greet',
-                    customData: { language: "en" },
-                    socketUrl: "http://34.131.236.128",
-                    // add other props here
-                  },
-                  null
-                );
-              }),
-              t.insertBefore(e, t.firstChild);
-          })();
-          </script>
+      <div class="container">
+          <div class="row">
+         
+
+            <div class="col-lg-6  main-page-content order-lg-1">
+            
+              <div class="d-flex flex-column bd-highlight mb-3">
+                <div class="p-2 bd-highlight"><h1>Medicbot</h1></div>
+                <div class="p-2 bd-highlight"><h3>For Healthcare Experts</h3></div> 
+              </div>
+             
+            </div>
+            <div class="col-lg-6 main-page-content order-lg-12">
+              <p>The Chatbot is trained to provide information<br> about a number of commonly used drugs. </p>
+            </div>
+           
+          </div>
+      </div>
+</div>
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     
