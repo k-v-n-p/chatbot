c65ab9fbf839a7a43c2c3fe4468baeedad05da14
Pranav K V N <kvnp179@gmail.com>
2022-01-18 03:56:56 +0530

    Update home.php
    gview

--- home.php
+++ home.php
@@ -30,22 +30,28 @@
 				$password=md5($_POST['password']);
 				$confirmPassword=md5($_POST['confirmPassword']);
 				if($password == $confirmPassword){
-					$check_query="SELECT * from users where email='".$email."'AND password='".$password."'limit 1";
-					$result=mysqli_query($link, $check_query);
-					if(mysqli_num_rows($result)>0){
-						echo "<script>  alert('Account already exists with this email.'); </script>";
-					}
-					else{
-						$insert_query="INSERT INTO users(username, email, password) 
-										VALUES('$username', '$email', '$password')";
-						$insert_result=mysqli_query($link, $insert_query);		
-						if(!$insert_result) {
-							echo "<script>  alert('Cannot create account. Please contact devs'); </script>";
+					$username_query="SELECT * from users where username='".$username."'limit 1";
+					$username_result=mysqli_query($link, $username_query);
+					if(mysqli_num_rows($username_result)>0){
+						echo "<script>  alert('Username already exists'); </script>";
+					}else{
+						$check_query="SELECT * from users where email='".$email."'AND password='".$password."'limit 1";
+						$result=mysqli_query($link, $check_query);
+						if(mysqli_num_rows($result)>0){
+							echo "<script>  alert('Account already exists with this email.'); </script>";
 						}
 						else{
-							//echo "<script>  alert('Registration successfull'); </script>";
-							$username="";
-							$email="";
+							$insert_query="INSERT INTO users(username, email, password) 
+											VALUES('$username', '$email', '$password')";
+							$insert_result=mysqli_query($link, $insert_query);		
+							if(!$insert_result) {
+								echo "<script>  alert('Cannot create account. Please contact devs'); </script>";
+							}
+							else{
+								echo "<script>  alert('Registration successfull, please login to contine'); </script>";
+								$username="";
+								$email="";
+							}
 						}
 					}
 
@@ -66,14 +72,57 @@
 				if(mysqli_num_rows($result)>0){
 					$row=mysqli_fetch_assoc($result);
 					$_SESSION['username']=$row['username'];
+					//IP store
+					$PublicIP = $_SERVER['REMOTE_ADDR'];
+					/*
+					echo 'IP: ' . $PublicIP. "\n";
+					echo 'Country Name: ' . $ipdat->geoplugin_countryName . "\n";
+					echo 'City Name: ' . $ipdat->geoplugin_city . "\n";
+
+
+					echo 'Latitude: ' . $ipdat->geoplugin_latitude . "\n";
+					echo 'Longitude: ' . $ipdat->geoplugin_longitude . "\n";
+					echo 'state: ' . $ipdat->geoplugin_region. "\n";
+					*/
+					$username=$_SESSION['username'];
+					$check_query="SELECT * from ips where username='".$username."'AND ip='".$PublicIP."' limit 1";
+					$result=mysqli_query($link, $check_query);
+					if(mysqli_num_rows($result)==0){
+						//no ip record yet.. add in both tables
+						$ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $PublicIP));
+						//t1
+						$insert_query="INSERT INTO ips(username, ip) VALUES('$username', '$PublicIP')";
+						$insert_result=mysqli_query($link, $insert_query);
+						//t2
+						
+						$addquery="INSERT INTO last_acc_loc(username, ip, city, state, country, latlon ) 
+									VALUES('$username', '$PublicIP', '$ipdat->geoplugin_city', '$ipdat->geoplugin_region', '$ipdat->geoplugin_countryName', 
+											concat('$ipdat->geoplugin_latitude', 'Q' ,'$ipdat->geoplugin_longitude') )";
+						$addresult=mysqli_query($link, $addquery);
+					}
+					else{
+						$ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $PublicIP));
+						//ip exists.. but recent data for user might need to get updated
+						$query1="SELECT * from last_acc_loc where username='".$username."'AND ip='".$PublicIP."' limit 1";
+						$res1=mysqli_query($link, $query1);
+						if(mysqli_num_rows($res1)==0){
+							$addquery="UPDATE last_acc_loc SET city = '".$ipdat->geoplugin_city."'
+											AND ip = '".$PublicIP."' 
+											AND state = '".$ipdat->geoplugin_region."' 
+											AND country = '".$ipdat->geoplugin_countryName."'
+											AND latlon=CONCAT_WS(',','$ipdat->geoplugin_latitude','$ipdat->geoplugin_longitude') 
+											WHERE username='".$username."'";
+								$addresult=mysqli_query($link, $addquery);
+						}
+					}
 					header("Location: welcome.php");
 				}
 				else{
 					echo '<script> document.getElementById("alr").innerHTML = "Incorrect credentials"; </script>';
 					//echo '<script>  alert('apsadfm'); </script>';
 				}
-
 			}
+								
 			
 		?>
  
